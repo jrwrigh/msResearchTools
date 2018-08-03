fff70f2f0248bc36fe7cfbded04af89513940740
James Wright <jrwrigh.iii@gmail.com>
2018-08-03 12:04:01 -0400

    corrected SBESRestart.sh. wasn't working previously

--- clusterUtils/SBESRestartQsub.sh
+++ clusterUtils/SBESRestartQsub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -N 20180727_AR1.30_phi4_octov2SBES
-#PBS -l select=2:ncpus=28:mpiprocs=28:mem=32gb:phase=16
+#PBS -l select=4:ncpus=28:mpiprocs=28:mem=32gb:phase=16
 #PBS -l walltime=72:00:00
 #PBS -j oe
 
@@ -8,7 +8,8 @@ module purge
 module add ansys/19.0
 module add intel/17.0
 
-INITFILE=20180727_AR1.30_phi4_octov2SBES_001.res
+DEFFILE=20180801_McD13_4S3_SBES.def
+RESTARTFILE=20180801_McD13_4S3_SBES_001.res
 
 cd $PBS_O_WORKDIR
 
@@ -18,13 +19,13 @@ echo "$machines \n"
 
 for node in `uniq $PBS_NODEFILE`
 do
-	ssh $node "cp $PBS_O_WORKDIR/$INITFILE $TMPDIR"
 	ssh $node "cp $PBS_O_WORKDIR/$DEFFILE $TMPDIR"
+	ssh $node "cp $PBS_O_WORKDIR/$RESTARTFILE $TMPDIR"
 done
 
 cd $TMPDIR
 
-cfx5solve -batch -continue-from-file $BACKUPFILE -file $INITFILE -double -par-dist "$machines" -job -output-summary-option 3 -verbose 
+cfx5solve -batch -continue-from-file $RESTARTFILE -def $DEFFILE -double -par-dist "$machines" -job -output-summary-option 3 -verbose -size 1.1
 
 for node in `uniq $PBS_NODEFILE`
 do
