758a501492a9f8cfd9667e0c73a211412035ce31
James Wright <jrwrigh.iii@gmail.com>
2018-09-03 15:18:04 -0400

    updated fluentSBES.sh script

--- clusterUtils/fluentSBES.sh
+++ clusterUtils/fluentSBES.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -N FluentSBES
-#PBS -l select=4:ncpus=28:mpiprocs=28:mem=32gb:phase=17
+#PBS -N PlenumMeshTestSBES
+#PBS -l select=3:ncpus=40:mpiprocs=40:mem=32gb:phase=18b
 #PBS -l walltime=72:00:00
 #PBS -j oe
 #PBS -m abe
@@ -12,14 +12,24 @@ module add intel/17.0
 
 set echo on 
 
+echo "#######################"
+echo "###SCRIPT FILE START###"
+echo "#######################"
+echo ""
+cat $0
+echo ""
+echo "#####################"
+echo "###SCRIPT FILE END###"
+echo "#####################"
+
 cd $PBS_O_WORKDIR
 
-fluentType=3ddp
-caseFile=McD13_4S3_SBES.cas
-initDataFile=3980746_McD13_4S3_SST_test.dat
+FLUENTTYPE=3ddp
+CASEFILE=PlenumMeshTestSBES4.cas
+INITDATAFILE=4038816_PlenumMeshTest_SST.dat
 
-dataFileName=McD13_4S3_SBES_test
-outfile=SBESTest.log
+DATAFILENAME=PlenumMeshTest_SBES
+OUTFILE=SBESTest.log
 
     # MPI options are [ibmmpi, intel, openmpi, cray]
 MPI=intel
@@ -27,12 +37,12 @@ MPI=intel
 ############
 jobid_num=$(echo $PBS_JOBID | grep -Eo "[0-9]{3,}")
 echo "jobid_num: $jobid_num"
-outFilePath="$PBS_O_WORKDIR/${jobid_num}_${outfile}"
-dataFileName=${jobid_num}_${dataFileName}.dat
+OUTFILEPATH="$PBS_O_WORKDIR/${jobid_num}_${OUTFILE}"
+DATAFILENAME=${jobid_num}_${DATAFILENAME}.dat
 
 ### Making the Journal file
-journalFile="$jobid_num"_FluentSBES.jou
-cat <<EOT >$journalFile
+JOURNALFILE="$jobid_num"_FluentSBES.jou
+cat <<EOT >$JOURNALFILE
 /file/set-batch-options
 ; confirm file overwrite?
 yes
@@ -40,22 +50,33 @@ yes
 yes
 ; Hide Questions?
 no
-/file/read-case $caseFile
-/file/read-data $initDataFile
+/file/read-case $CASEFILE
+/file/read-data $INITDATAFILE
 /solve/initialize/init-instantaneous-vel
+; 24= Coupled
+; /solve/set/p-v-coupling 24
 define/parameters/input-parameters/edit "TimeStepSize"
 
 5e-6
 /define/parameters/input-parameters/edit "rotationalVelocity"
 
-0
+916.7
+/solve/monitors/residual/convergence-criteria
+.0001
+.0001
+.0001
+.0001
+
+
+
+
 /server/start-server server_info.txt
 !date
 /solve/dual-time-iterate 40000
 
 !date
 /parallel/timer/usage
-/file/write-data $dataFileName
+/file/write-data $DATAFILENAME
 exit
 yes
 EOT
@@ -83,14 +104,14 @@ Start Time : $(date)
 Inputs:
 -------------
 Journal File = $JOURNALFILE
-Case File = $caseFile
-Fluent Verison = $fluentType
+Case File = $CASEFILE
+Fluent Verison = $FLUENTTYPE
 
 
 Output Files:
 --------------
-Data File = $dataFileName
-Log File = $outfile
+Data File = $DATAFILENAME
+Log File = $OUTFILE
 
 |                           | 
 +---------------------------+
@@ -99,14 +120,14 @@ Log File = $outfile
 
 for node in `uniq $PBS_NODEFILE`
 do
-	ssh $node "cp $PBS_O_WORKDIR/$caseFile $TMPDIR"
-	ssh $node "cp $PBS_O_WORKDIR/$initDataFile $TMPDIR"
+	ssh $node "cp $PBS_O_WORKDIR/$CASEFILE $TMPDIR"
+	ssh $node "cp $PBS_O_WORKDIR/$INITDATAFILE $TMPDIR"
 	ssh $node "mv $PBS_O_WORKDIR/$JOURNALFILE $TMPDIR"
 done
 
 cd $TMPDIR
 
-fluent $fluentType $fluent_args > $outFilePath
+fluent $FLUENTTYPE $fluent_args > $OUTFILEPATH
 
 
 for node in `uniq $PBS_NODEFILE`
