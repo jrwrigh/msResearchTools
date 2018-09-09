05505ccf4cd2e144ade42d358406c204a4ec1143
James Wright <jrwrigh.iii@gmail.com>
2018-09-09 11:16:02 -0400

    updated fluent animation script with colormap, pressure, and density

--- clusterUtils/fluentSBESAnimation.sh
+++ clusterUtils/fluentSBESAnimation.sh
@@ -32,6 +32,10 @@ dataFilename=PlenumMeshTest_SBES
 outFile=SBESTest.log
 imgDirName=images
 
+colormapscheme=rw_colormap.scm
+colormapfile=blackbody_extended.colormap
+fluentcolormap=blkbdy-ext
+
     # MPI options are [ibmmpi, intel, openmpi, cray]
 MPI=intel
 
@@ -77,6 +81,9 @@ define/parameters/input-parameters/edit "TimeStepSize"
 ; #############
 ; Start Visualization stuff
 ; #############
+(load "rw_colormap.scm")
+/file/read-colormap $colormapfile
+/display/set/colormap $fluentcolormap
 /display/set/contours/filled-contours yes
 /display/set/picture/driver png
 /display/set/picture/landscape yes
@@ -84,28 +91,50 @@ define/parameters/input-parameters/edit "TimeStepSize"
 /display/set/picture/x-resolution 1200
 /display/set/picture/y-resolution 720
 /display/set/picture/color-mode color
+/display/set/colors/color-scheme classic
+/display/set/colors/background "dark gray"
+/display/set/picture/invert-background? no
 /surface/plane-point-n-normal zyplane 0 0 0 1 0 0
 
 /display/set/lights/headlight-on? no
 /display/set/lights/lights-on? no
+
 /display/set/contours/surfaces zyplane ()
 /display/set/contours/coloring no
 /display/set/contours/node-values? yes
+/display/set/contours/clip-to-range? no
+/display/set/windows/scale/alignment bottom
+/display/set/windows/text/application? no
+/display/set/windows/text/date? no
+/display/set/windows/text/visible? yes
+/display/set/windows/text/top -0.85
 
-/display/contour density 1 2.2
+/display/contour density 0.7 2.2
 /display/views/restore-view right
 /display/views/auto-scale
+/display/views/camera/zoom-camera 1.8
+/display/views/camera/target 0 0 0.26
 /display/save-picture ${imgDirName}/density0.png
+/display/contour pressure -30000 80000
+/display/views/restore-view right
+/display/views/auto-scale
+/display/views/camera/zoom-camera 1.8
+/display/views/camera/target 0 0 0.26
+/display/save-picture ${imgDirName}/pressure0.png
 
 ; Make pictures at every timestep
-/solve/execute-commands/add-edit command-1 1 "time-step" "/display/contour density 1 2.2"
+/solve/execute-commands/add-edit command-1 1 "time-step" "/display/contour density 0.7 2.2"
 /solve/execute-commands/add-edit command-2 1 "time-step" "/display/views/restore-view right"
 /solve/execute-commands/add-edit command-3 1 "time-step" "/display/views/auto-scale"
-; /solve/execute-commands/add-edit command-4 1 "time-step" "/display/set/picture/x-resolution 960"
-; /solve/execute-commands/add-edit command-5 1 "time-step" "/display/set/picture/y-resolution 720"
-; /solve/execute-commands/add-edit command-6 1 "time-step" "/display/re-render"
-/solve/execute-commands/add-edit command-7 1 "time-step" "/display/save-picture ${imgDirName}/density%t.png"
-
+/solve/execute-commands/add-edit command-4 1 "time-step" "/display/views/camera/zoom-camera 1.8"
+/solve/execute-commands/add-edit command-5 1 "time-step" "/display/views/camera/target 0 0 0.26"
+/solve/execute-commands/add-edit command-6 1 "time-step" "/display/save-picture ${imgDirName}/density%t.png"
+/solve/execute-commands/add-edit command-7 1 "time-step" "/display/contour pressure -35000 80000"
+/solve/execute-commands/add-edit command-8 1 "time-step" "/display/views/restore-view right"
+/solve/execute-commands/add-edit command-9 1 "time-step" "/display/views/auto-scale"
+/solve/execute-commands/add-edit command-10 1 "time-step" "/display/views/camera/zoom-camera 1.8"
+/solve/execute-commands/add-edit command-11 1 "time-step" "/display/views/camera/target 0 0 0.26"
+/solve/execute-commands/add-edit command-12 1 "time-step" "/display/save-picture ${imgDirName}/pressure%t.png"
 
 /solve/dual-time-iterate 1000
 
@@ -157,6 +186,8 @@ for node in `uniq $PBS_NODEFILE`
 do
 	ssh $node "cp $PBS_O_WORKDIR/$caseFile $TMPDIR"
 	ssh $node "cp $PBS_O_WORKDIR/$initDataFile $TMPDIR"
+	ssh $node "cp $PBS_O_WORKDIR/$colormapscheme $TMPDIR"
+	ssh $node "cp $PBS_O_WORKDIR/$colormapfile $TMPDIR"
 	ssh $node "mv $PBS_O_WORKDIR/$journalFile $TMPDIR"
 	ssh $node "mkdir $TMPDIR/$imgDirName"
 done
