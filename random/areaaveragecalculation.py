3704e35609c45e259fcfccd034b3f5f303ecfde3
u2berggeist <jrwrigh.iii@gmail.com>
2018-09-15 11:10:44 -0400

    area average calc now updated and fully working.

--- random/areaaveragecalculation.py
+++ random/areaaveragecalculation.py
@@ -1,8 +1,25 @@
 from sympy import *
-from sympy.abc import r, t, v, R
+from sympy.abc import r, t
 
-init_printing(use_unicode=False, wrap_line=False)
+init_printing(use_unicode=True, wrap_line=True)
 w = Symbol('w', positive=True)
+v = Symbol('v', positive=True)
+R = Symbol('R', positive=True)
+U = Symbol('U', positive=True)
+
+R = .0254
+U = 40.197
+w = 916.7
+
 func = sqrt((w*r)**2 + v**2)*r
 
-integrate(func, (r, 0, R))
+intfunc = integrate(integrate(func, (r, 0, R)), (t, 0, 2*pi))
+
+finfunc = intfunc/(R**2*pi) - U
+
+pprint(finfunc)
+
+vafunc = solve(finfunc, v)
+
+pprint(vafunc)
+
