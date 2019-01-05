1f69fa91c01ed97f58bede96e30155f87731f660
James Wright <jrwrigh.iii@gmail.com>
2019-01-05 18:41:09 -0500

    Massive update to fluentSST to match fluentSBES

--- clusterUtils/fluentSST.sh
+++ clusterUtils/fluentSST.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -N PlenumMeshTestSST5_S0
-#PBS -l select=2:ncpus=20:mpiprocs=20:mem=32gb:phase=11a
+#PBS -N ERCOFTAC_m4c1_SST
+#PBS -l select=1:ncpus=40:mpiprocs=40:mem=32gb
 #PBS -l walltime=02:00:00
 #PBS -j oe
 #PBS -m abe
@@ -10,6 +10,8 @@ module purge
 module add ansys/19.0
 module add intel/17.0
 
+echo $SHELL
+SHELL="/bin/bash"
 set echo on 
 
 echo "###SCRIPT FILE START###"
@@ -18,23 +20,51 @@ echo "###SCRIPT FILE END###"
 
 cd $PBS_O_WORKDIR
 
-FLUENTTYPE=3ddp
-CASEFILE=PlenumMeshTestSST4.cas
+#####################################################################
+#                         Parameters
 
-DATAFILENAME=PlenumMeshTest_SST
-OUTFILE=PlenumMeshTest_SST.log
+caseFile=ERCOFTAC_m4c1_SST.cas
+dataFileName=ERCOFTAC_m4c1_SST
+    
+num_iterations=1800
 
-    # MPI options are [ibmmpi, intel, openmpi, cray]
-MPI=intel
+SWITCH_ROTATIONAL_VELOCITY=false
+rotationalVelocity=0
 
-############
+# Fluent parameters
+mpi=intel # MPI options are [ibmmpi, intel, openmpi, cray]
+fluentType=3ddp
+
+#####################################################################
+#                     Fluent Journal File Logic
+
+if $SWITCH_ROTATIONAL_VELOCITY; then
+    rotationalVelocityParameter="/define/parameters/input-parameters/edit \"rotationalVelocity\"
+
+    $rotationalVelocity"
+else
+    rotationalVelocityParameter=""
+fi
+
+#              ^^END Fluent Journal File Logic END^^
+#####################################################################
+
+#####################################################################
+#                     Name Wrangling
+
+num_nodes=$(cat $PBS_NODEFILE | sort -u | wc -l)
+tot_cpus=$(cat $PBS_NODEFILE | wc -l )
 jobid_num=$(echo $PBS_JOBID | grep -Eo "[0-9]{3,}")
-OUTFILEPATH="$PBS_O_WORKDIR/${jobid_num}_${OUTFILE}"
-DATAFILENAME=${jobid_num}_${DATAFILENAME}.dat
 
-### Making the Journal file
-JOURNALFILE="$jobid_num"_FluentSST.jou
-cat <<EOT >$JOURNALFILE
+dataFileName=${jobid_num}_${dataFileName}
+outFilePath="$PBS_O_WORKDIR/${dataFileName}.log"
+journalFile="$jobid_num"_SST.jou
+outDirName=$dataFileName
+
+#####################################################################
+#                     Journal File Creation
+
+cat <<EOT >$journalFile
 /file/set-batch-options
 ; confirm file overwrite?
 yes
@@ -42,10 +72,8 @@ yes
 yes
 ; Hide Questions?
 no
-/file/read-case $CASEFILE
-/define/parameters/input-parameters/edit "rotationalVelocity"
-
-0
+/file/read-case $caseFile
+$rotationalVelocityParameter
 /solve/monitors/residual/convergence-criteria
 .00001
 .00001
@@ -57,64 +85,89 @@ no
 
 
 !date
-/solve/iterate 1800
+/solve/iterate $num_iterations
 !date
-/file/write-data $DATAFILENAME
+/file/write-data $dataFileName
 exit
 yes
 EOT
+#              ^^END Journal File Creation END^^
+#####################################################################
 
-num_nodes=$(cat $PBS_NODEFILE | sort -u | wc -l)
-echo "\$num_nodes = " $num_nodes
-tot_cpus=$(cat $PBS_NODEFILE | wc -l )
-echo "\$tot_cpus = " $tot_cpus
+#####################################################################
+#                     Building Fluent Arguements
 
 fluent_args="-t${tot_cpus} $fluent_args -cnf=$PBS_NODEFILE"
 
-fluent_args="-g -i $JOURNALFILE -mpi=$MPI $fluent_args"
+fluent_args="-g -i $journalFile -mpi=$mpi $fluent_args"
+
+#####################################################################
+#                     Printing Job Information
 
 echo "
-+--------------+
-| START SOLVER | 
-+--------------+
++--------------------------+
+|       START SOLVER       | 
++--------------------------+
 
-+---------------------------+
-|                           | 
++-------------------------------------+
+|                                     | 
 
 Start Time : $(date)
 
+Job Information:
+----------------
+Num. CPUS = $tot_cpus
+Num. Nodes = $num_nodes
+Job ID # = $jobid_num
 
 Inputs:
--------------
-Journal File = $JOURNALFILE
-Case File = $CASEFILE
-Fluent Verison = $FLUENTTYPE
+----------------
+Journal File = $journalFile
+Case File = $caseFile
+Fluent Verison = $fluentType
 
 
 Output Files:
---------------
-Data File = $DATAFILENAME
-Log File = $OUTFILE
+-----------------
+Data File = ${dataFileName}.dat
+Log File = ${dataFileName}.log
 
-|                           | 
-+---------------------------+
+Directories:
+------------
+PBS_O_WORKDIR = $PBS_O_WORKDIR
+TMPDIR = $TMPDIR
+
+Other:
+-------
+Nodes = $(uniq $PBS_NODEFILE)
+
+|                                     | 
++-------------------------------------+
 
 "
 
+#####################################################################
+#                     Running the Job Itself
+
 for node in `uniq $PBS_NODEFILE`
 do
-	ssh $node "cp $PBS_O_WORKDIR/$CASEFILE $TMPDIR"
-	ssh $node "mv $PBS_O_WORKDIR/$JOURNALFILE $TMPDIR"
+	ssh $node "cp $PBS_O_WORKDIR/$caseFile $TMPDIR"
+	ssh $node "mv $PBS_O_WORKDIR/$journalFile $TMPDIR"
 done
 
 cd $TMPDIR
+printf "Contents of TMPDIR\n"
+ls -la
+printf "\n\n"
 
-fluent $FLUENTTYPE $fluent_args > $OUTFILEPATH
+fluent $fluentType $fluent_args > $outFilePath
 
+printf "Simulation Completed\n"
 
 for node in `uniq $PBS_NODEFILE`
 do
-    ssh $node "cp -r $TMPDIR/* $PBS_O_WORKDIR"
+    ssh $node "mkdir $PBS_O_WORKDIR/$outDirName"
+    ssh $node "cp -r $TMPDIR/* $PBS_O_WORKDIR/$outDirName"
 done
 
 
