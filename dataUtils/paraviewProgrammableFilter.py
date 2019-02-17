17598c6d07eafe6f8d99a2ad9e68b880746a576d
u2berggeist <jrwrigh.iii@gmail.com>
2019-02-17 16:27:48 -0500

    Add Vector data creation to paraview python script
    Added creation of cartesian vectors (Mean and RMSE) as well as the
    Cylindrical vectors.

--- dataUtils/paraviewProgrammableFilter.py
+++ dataUtils/paraviewProgrammableFilter.py
@@ -4,7 +4,7 @@ import numpy as np
 input0 = inputs[0]
 
 ############################################
-#           CREATING DATA
+#        CREATING COORDINATE DATA
 ############################################
 
 coordVec = input0.Points.Arrays[0]
@@ -32,6 +32,35 @@ thetaVec = algs.cross(zVec, radVec)
 thetaVec = algs.make_vector(*thetaVec.T)
 thetaVec = algs.norm(thetaVec)
 
+############################################
+#      CREATING CARTESIAN VECTOR DATA
+############################################
+VelMeanVec = algs.make_vector(input0.PointData['Mean_X_Velocity'].Arrays[0],
+                              input0.PointData['Mean_Y_Velocity'].Arrays[0],
+                              input0.PointData['Mean_Z_Velocity'].Arrays[0])
+
+VelRMSEVec = algs.make_vector(input0.PointData['RMSE_X_Velocity'].Arrays[0],
+                              input0.PointData['RMSE_Y_Velocity'].Arrays[0],
+                              input0.PointData['RMSE_Z_Velocity'].Arrays[0])
+
+############################################
+#      CREATING CYLINDRICAL VECTOR DATA
+############################################
+VelocityVec = input0.PointData['Velocity'].Arrays[0]
+
+CylVec = [radVec, thetaVec, zVec]
+VectorDict = {
+    "VelCyl": VelocityVec,
+    "VelCylMean": VelMeanVec,
+    "VelCylRMSE": VelRMSEVec
+}
+
+VectorCylDict = {}
+for key in VectorDict.keys():
+    VectorCylDict[key] = algs.make_vector(
+        algs.dot(VectorDict[key], radVec), algs.dot(VectorDict[key], thetaVec),
+        algs.dot(VectorDict[key], zVec))
+
 ###########################################
 #            OUTPUTING DATA
 ###########################################
@@ -45,6 +74,15 @@ output.PointData.append(z, 'z')
 output.PointData.append(radVec, 'rVec')
 output.PointData.append(zVec, 'zVec')
 output.PointData.append(thetaVec, 'thetaVec')
+
+# Cartesian Vector Data
+output.PointData.append(VelMeanVec, 'Velocity_Mean')
+output.PointData.append(VelRMSEVec, 'Velocity_RMSE')
+
+# Cylindrical Vector Data
+output.PointData.append(VectorCylDict['VelCyl'], 'VelocityCyl')
+output.PointData.append(VectorCylDict['VelCylMean'], 'VelocityCyl_Mean')
+output.PointData.append(VectorCylDict['VelCylRMSE'], 'VelocityCyl_RMSE')
 
 # for key in input0.PointData.keys():
 #     output.PointData.append(input0.PointData[key], key
