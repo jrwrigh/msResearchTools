3bcb28dff149029807ffac7aa07ba25aa3d9e858
jrwrigh <jrwrigh.iii@gmail.com>
2019-02-19 13:41:07 -0500

    Create paraviewServerLaunch.sh
    Script created to launch Paraview server on the cluster.

---
+++ clusterUtils/paraviewServerLaunch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to assist the launching of a Paraview server on the Palmetto Cluster
+# Usage:
+# 	1) Run script to start the interactive session on the cluster
+#	2) Run the script again (in the interactive session) to start the Paraview server
+
+simgPath="~/paraview_5.6.0RC3-egl.simg"
+nodehost=${HOSTNAME:0:4} 
+
+if [ "$nodehost" == "node" ]; then
+	module load singularity
+	export DISPLAY=:0
+	export SINGULARITY="$(which singularity) exec -B /common/miller/jrwrigh:/common -B /usr/lib64/nvidia/:/.singularity.d/libs -B /etc/machine-id:/etc/machine-id -B /run/user:/run/user -B /usr/bin:/host_bin -B $(pwd):/host_pwd $simgName" 
+	${SINGULARITY} pvserver -display :0
+else 
+	qsub -I -X -l select=1:ncpus=2:ngpus=1:mem=20gb,walltime=4:00:00
+fi 
+
