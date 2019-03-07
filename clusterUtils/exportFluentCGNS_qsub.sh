48e11a012887a7c3b774c5f75139734469380459
James Wright <jrwrigh.iii@gmail.com>
2019-03-07 10:43:19 -0500

    Create exportFluentCGNS scripts
    Created the two exportFluentCGNS scipts. These include the jobscript,
    which contains the required journal file and is what `qsub` actually
    runs, and the qsub, which should be aliased as `exportFluentCGNS`. The
    two files together allow CGNS datafiles to be exported without using a
    GUI.

---
+++ clusterUtils/exportFluentCGNS_qsub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# NOTE: This script requires the associated PBS Job Script to work along side it.
+
+jobScriptPath="/home/jrwrigh/gitRepos/msresearchtools/clusterUtils/exportFluentCGNS_jobscript.sh"
+jobScriptName=$(basename $jobScriptPath)
+
+    # copy the PBS job script to the current directory
+cp $jobScriptPath .
+
+    # copy the casePath and initDataPath names into the jobScript
+sed -i "s/casePathNameThing/$1/" $jobScriptName
+sed -i "s/initDataPathNameThing/$2/" $jobScriptName
+
+qsub $jobScriptName
+
+rm $jobScriptName
