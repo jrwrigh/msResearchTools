c1346df947eb11aa1952ffa8eb6a0e1334c82a3e
James Wright <jrwrigh.iii@gmail.com>
2019-03-25 22:10:14 -0400

    Add PuTTY note to paraviewServerLaunch
    Added a message that will display the PuTTY command needed to do the
    port forwarding for launching paraview.

--- clusterUtils/paraviewServerLaunch.sh
+++ clusterUtils/paraviewServerLaunch.sh
@@ -8,12 +8,19 @@
 
 simgPath="paraview_5.6.0RC3-egl.simg"
 nodehost=${HOSTNAME:0:4} 
+nodenumber=${HOSTNAME:5:9}
 
 if [ "$nodehost" == "node" ]; then
     module unload python
 	module load singularity
 	export DISPLAY=:0
 	export SINGULARITY="$(which singularity) exec -B /common/miller/jrwrigh:/common -B /usr/lib64/nvidia/:/.singularity.d/libs -B /etc/machine-id:/etc/machine-id -B /run/user:/run/user -B /usr/bin:/host_bin -B $(pwd):/host_pwd ${simgPath}" 
+
+    echo "
+    Command for PuTTY SSH port forwarding:
+    putty -ssh -L 10000:$HOSTNAME:11111 jrwrigh@login.palmetto.clemson.edu
+    "
+
 	${SINGULARITY} pvserver -display :0
  
 else 
