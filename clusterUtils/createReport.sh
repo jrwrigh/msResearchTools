d4caea573334f624e4db81194ce6ff8ae841dcaf
James Wright <jrwrigh.iii@gmail.com>
2019-04-22 21:33:19 -0400

    Add script to create CFD Post Report
    - Script to create report for CFD Post

---
+++ clusterUtils/createReport.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Arguements: [SST|SBES] [data file]
+
+dataFile=$2
+reportStateDirPath='/common/miller/jrwrigh/DesignSpaceSims/cfdpost_statefiles'
+
+jobScriptName=createReport_jobscript.sh
+if [ $1 = "SST" ]; then
+    reportStateFilePath=$(ls $reportStateDirPath/*SST*.cst)
+else
+    reportStateFilePath=$(ls $reportStateDirPath/*SBES*.cst)
+fi
+
+reportFileName=$(basename $dataFile .cdat)_Report
+
+echo cfdpost -report $reportStateFilePath -name $reportFileName -outdir $PBS_O_WORKDIR ${PBS_O_WORKDIR}$dataFile
+
+cfdpost -report $reportStateFilePath -name $reportFileName -outdir $PBS_O_WORKDIR ${PBS_O_WORKDIR}$dataFile
+
